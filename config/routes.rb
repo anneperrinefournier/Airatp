cda400505fbc622536a22974d02565d9bcda4214
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-21 12:24:11 +0100

    page home back

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :vehicules
-  resources :booking, only: %i[index create show destroy]
+  resources :vehicles
+  resources :bookings, only: %i[index create show destroy]
 end
