d7f4c79b66d725cffbc112302d39ceb6f455e792
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2023-11-21 13:17:52 +0100

    Bugfix vehicles index

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,10 +2,10 @@ class VehiclesController < ApplicationController
   def show
     @vehicle = Vehicle.find(params[:id])
   end
-  
+
   def index
-    if params[:search]
-      @vehicles = Vehicle.where(vehicle_type: params[:search])
+    if params[:vehicle_type]
+      @vehicles = Vehicle.where(vehicle_type: params[:vehicle_type])
     else
       @vehicles = Vehicle.all
     end
