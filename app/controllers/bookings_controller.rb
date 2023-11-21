109b490ed64dc251800dfbc71d6689c19b3815dc
pierrefrancois thomas <pf-thomas@hotmail.fr>
2023-11-21 15:51:23 +0100

    create a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,22 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def create
+    @vehicle = Vehicle.find(params[:vehicle_id])
+    @booking = @vehicle.bookings.new(booking_params)
+    @booking.user = current_user
+    @booking.status = :pending
+    if @booking.save!
+      redirect_to bookings_path, notice: 'Booking was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
+  end
 end
