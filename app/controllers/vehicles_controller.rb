144a53b5a6c1fbe3e49143f6b6ab3df55f5ab4ff
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2023-11-21 13:09:12 +0100

    Ajout de l'index

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,9 @@
 class VehiclesController < ApplicationController
+  def index
+    if params[:search]
+      @vehicles = Vehicle.where(vehicle_type: params[:search])
+    else
+      @vehicles = Vehicle.all
+    end
+  end
 end
