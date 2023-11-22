eadcb069dd982fa82d91f2e5dee3be1069a2487f
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-22 17:52:40 +0100

    js number passengers en cours

---
+++ app/javascript/controllers/travelers_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="travelers"
+export default class extends Controller {
+  static targets = ['numberOfTravelers'];
+
+  connect() {
+  }
+
+  search() {
+    numberofPassengers = this.numberOfTravelers
+
+    vehicle[passengers_capacity.value]
+ }
+}
