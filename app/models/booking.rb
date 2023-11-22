0d131083bad6b5b9811e5b93fc58ae8c90b34167
henri guillot <henri.guillot@hotmail.com>
2023-11-22 13:13:52 +0100

    modification of dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,4 +7,18 @@ class Booking < ApplicationRecord
     validated: 1,
     refused: 2
   }
+
+  def self.status_color_class(status)
+    case status.to_sym
+    when :pending
+      "text-grey"
+    when :validated
+      "text-green"
+    when :refused
+      "text-red"
+    else
+      ""
+    end
+  end
+
 end
