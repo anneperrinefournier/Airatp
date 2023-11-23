39f5b3c39cc9a4b614174bbe3e8923ca1357b1e2
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-23 17:21:17 +0100

    navbar finito

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -5,16 +5,41 @@ class VehiclesController < ApplicationController
   end
 
   def index
-    if params[:vehicle_type]
+    if params[:vehicle_type].present?
       @vehicles = Vehicle.where(vehicle_type: params[:vehicle_type])
+    elsif params[:queryname].present? && params[:passengers_capacity].present?
+      @vehicles = Vehicle.where("name ILIKE ?", "%#{params[:queryname]}%")
+                         .merge(passengers_capacity_range)
+
+    elsif params[:queryname].present?
+      @vehicles = Vehicle.where("name ILIKE ?", "%#{params[:queryname]}%")
+    elsif params[:passengers_capacity].present?
+      passengers_capacity_range
     else
       @vehicles = Vehicle.all
     end
   end
+end
 
-  def search_by_passengers
-    number_of_passengers = params[:number_of_passengers]
-    @vehicles = Vehicle.where(passengers_capacity: number_of_passengers)
-    render json: @vehicles
-  end
+
+def passengers_capacity_range
+  capacity = params[:passengers_capacity].to_i
+      case capacity
+      when 1
+        @vehicles = Vehicle.where(passengers_capacity: capacity)
+      when 2..5
+        @vehicles = Vehicle.where('passengers_capacity BETWEEN ? AND 5', capacity)
+      when 6..10
+        @vehicles = Vehicle.where('passengers_capacity BETWEEN ? AND 10', capacity)
+      when 11..50
+        @vehicles = Vehicle.where('passengers_capacity BETWEEN ? AND 50', capacity)
+      when 51..100
+        @vehicles = Vehicle.where('passengers_capacity BETWEEN ? AND 100', capacity)
+      when 101..200
+        @vehicles = Vehicle.where('passengers_capacity BETWEEN ? AND 200', capacity)
+      when 201..300
+        @vehicles = Vehicle.where('passengers_capacity BETWEEN ? AND 300', capacity)
+      else
+        @vehicles = Vehicle.where('passengers_capacity >= ?', capacity)
+      end
 end
