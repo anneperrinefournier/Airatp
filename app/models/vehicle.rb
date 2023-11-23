5e7b9fe73f7b0eb6cb2c22f559fd7a8658640933
henri guillot <henri.guillot@hotmail.com>
2023-11-23 10:48:18 +0100

    added gem geocoder

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,6 @@ class Vehicle < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :bookings
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
