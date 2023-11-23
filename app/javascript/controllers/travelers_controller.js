4d613650414c5fb4ec61b95dc5ec775365801e39
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-23 11:12:09 +0100

    navbar style css

--- app/javascript/controllers/travelers_controller.js
+++ app/javascript/controllers/travelers_controller.js
@@ -7,21 +7,4 @@ export default class extends Controller {
 
   connect() {
   }
-
-  search() {
-    const numberOfPassengers = this.numberOfTravelers
-
-    Rails.ajax({
-      type: "GET",
-      url: `/vehicles/search_by_passengers?number_of_passengers=${numberOfPassengers}`,
-      dataType: "json",
-      success: (data) => {
-        // Traitement des données retournées
-        console.log(data);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
-  }
  }
